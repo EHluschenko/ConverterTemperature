e926cdc46ee472dee807398a529570dc51650735
Ed Gluschenko <edgluschenko3@gmail.com>
2025-02-06 15:53:37 +0200

    add converter Fahrenheit to degrees Celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,5 +3,12 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Converter temperature version 1.0");
+        double far=40;
+        double cel=convFarToCel(far);
+        System.out.println("Converter Fahrenheit to degrees Celsius");
+        System.out.println("Result is: "+cel+"°C");
+    }
+    public static double convFarToCel(double far){
+        return (far-32)/1.8;
     }
 }
